72e0b5aeeae36daa286758d6761c51d36e7c7a6a
kehindeomotayo <kennyjay_2@yahoo.com>
2024-03-12 16:11:41 +0000

    upadated vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
-# Create a VPC
-resource "aws_vpc" "VPC-Testing" {
-  cidr_block = "10.0.0.0/16"
- instance_tenancy = "default"
-  tags = {
-  Name = "VPC-Testing"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
 }
