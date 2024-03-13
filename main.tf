ea3fa89d2e7f7a2a4a333bb28e5304de890264d4
kehindeomotayo <kennyjay_2@yahoo.com>
2024-03-13 16:56:30 +0000

    new updated vpc

--- main.tf
+++ main.tf
@@ -1,18 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-1"
+  region = "us-west-1"
 }
 
 # Create a VPC
-resource "aws_vpc" "VPC" {
+resource "aws_vpc" "MY-VPC" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "MY-VPC"
+  }
 }
